b4f63b9e79b624b17f17f66333d680b3ea1c8260
jeremy <jeremy@ubuntu>
2014-06-05 09:16:47 -0700

    first commit

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -4,8 +4,9 @@
 class UsersController extends AppController {
 
     public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('add');
+    parent::beforeFilter();
+    // Allow users to register and logout.
+    $this->Auth->allow('add', 'logout');
     }
 
     public function index() {
@@ -67,5 +68,25 @@ class UsersController extends AppController {
         $this->Session->setFlash(__('User was not deleted'));
         return $this->redirect(array('action' => 'index'));
     }
+    
+    
+
+
+
+    public function login() {
+        if ($this->request->is('post')) {
+            if ($this->Auth->login()) {
+                return $this->redirect($this->Auth->redirect());
+            }
+            $this->Session->setFlash(__('Invalid username or password, try again'));
+        }
+    }
+
+    public function logout() {
+        return $this->redirect($this->Auth->logout());
+    }
+
+    
+    
 
 }
